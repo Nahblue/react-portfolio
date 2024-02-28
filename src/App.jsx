3fcb4ec6cd1b596cc92a75dc21904d1f48941f1b
nahblue <ana.paula.alves11@gmail.com>
2024-02-28 19:09:21 -0300

    ajustes de layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,83 +2,111 @@ import { GoArrowUpRight } from "react-icons/go";
 import { Tech } from "./components/Tech";
 import { Work } from "./components/Work";
 
+import data from './data/data.json'
+
 export function App() {
+  const techs = [
+    "Javascript",
+    "React",
+    "Tailwind",
+    "Styled Components",
+    "Node.js",
+  ]
+
+  const works = data.works
+
   return (
-    <div className="mx-auto   overflow-y-scroll no-scrollbar">
-      <nav className="flex max-w-6xl mx-auto justify-between p-8 bg-slate-50">
-        <a href="#" className="font-bold text-xl text-slate-900">Ana P</a>
+    <div className="mx-auto w-full h-screen overflow-y-scroll no-scrollbar relative scroll-smooth">
+      <div id="home" />
+      <nav className="flex max-w-6xl mx-auto justify-between p-8 bg-transparent sticky top-0">
+        <a href="#home" className="font-bold text-xl text-slate-900 hover:text-slate-600">Ana P</a>
         <ul className="flex gap-8 font-medium text-slate-500">
-          <li><a href="#" className="hover:text-slate-800">Projetos</a></li>
+          <li><a href="#projects" className="hover:text-slate-800">Projetos</a></li>
           <li><a href="#" target="_blank" className="flex justify-center items-center hover:text-slate-800">CV <GoArrowUpRight /></a></li>
         </ul>
       </nav>
 
-      <main className="p-8 max-w-6xl mx-auto flex flex-col">
-        <section className="flex flex-col gap-12 ">
-          <div className="flex justify-between gap-32">
-            <div className="flex flex-col gap-4">
-              <span>Olá, eu sou</span>
-              <h1 className="text-6xl font-medium max-w-100">
-                Ana Paula, <br />
-                desenvolvedora <br />
-                front-end
-              </h1>
-              <div>
-                <p className="flex gap-1"> 
-                  Cursando Análise e Desenvolvimento de Sistemas na <a href="#" className="flex items-center underline hover:text-slate-600">Estácio<GoArrowUpRight /></a>
-                </p>
-                <p className="flex gap-1"> 
-                  Estudante da <a href="#" className="flex items-center underline hover:text-slate-600">Rocketseat<GoArrowUpRight /></a>
-                </p>
-                <p className="flex gap-1"> 
-                  Graduada em Design Gráfico na <a href="#" className="flex items-center underline hover:text-slate-600">Estácio<GoArrowUpRight /></a>
-                </p>
+      <main className="w-full flex flex-col gap-44">
+        <section 
+           
+          className="w-full p-9"
+        >
+          <div className="max-w-[950px] mx-auto flex flex-col gap-12">
+            <div className="mt-9 flex justify-between gap-32">
+              <div className="flex flex-col gap-4">
+                <span>Olá, eu sou</span>
+                <h1 className="text-6xl font-medium max-w-100">
+                  Ana Paula, <br />
+                  desenvolvedora <br />
+                  front-end
+                </h1>
+
+                <div>
+                  <p className="flex gap-1"> 
+                    Cursando Análise e Desenvolvimento de Sistemas na <a href="https://estacio.br/" target="_blank" className="flex items-center underline hover:text-slate-600">Estácio<GoArrowUpRight /></a>
+                  </p>
+
+                  <p className="flex gap-1"> 
+                    Estudante da <a href="https://www.rocketseat.com.br/" target="_blank" className="flex items-center underline hover:text-slate-600">Rocketseat<GoArrowUpRight /></a>
+                  </p>
+
+                  <p className="flex gap-1"> 
+                    Graduada em Design Gráfico na <a href="https://estacio.br/" target="_blank" className="flex items-center underline hover:text-slate-600">Estácio<GoArrowUpRight /></a>
+                  </p>
+                </div>
               </div>
+              <img src="https://github.com/Nahblue.png" alt="" className="w-64 h-64 rounded-full" />
+          
             </div>
+          
 
-            <img src="https://github.com/Nahblue.png" alt="" className="w-64 h-64 rounded-full" />
-          </div>
+            <div className="flex flex-col gap-6">
+              <h2 className="text-3xl font-medium">Skills</h2>
 
-          <div className="flex flex-col gap-6">
-            <h2 className="text-3xl font-medium">Skills</h2>
-
-            <div className="flex gap-3">
-              <Tech title="Javascript" />
-              <Tech title="CSS" />
-              <Tech title="React" />
+              <div className="flex gap-3">
+                { techs.map((tech) => {
+                  return <Tech title={tech} key={tech} />
+                })}
+              </div>
             </div>
           </div>
         </section>
 
-        <section className="mt-48">
-          <div className="grid grid-flow-col gap-12 justify-between mt-20 ">
-            <div className="w-64 ">
-              <h2 className="text-3xl font-medium">Projetos</h2>
-              <p>quote aleatória</p>
-            </div>
+        <section 
+          id="projects"
+          className="w-full bg-slate-100 p-9"
+        >
+          <div className="max-w-[950px] mx-auto">
+            <div className="grid grid-flow-col gap-12 justify-between mt-20 mb-20 ">
+              <div className="w-64">
+                <h2 className="text-3xl font-medium">Projetos</h2>
+                <p>quote aleatória</p>
+              </div>
 
-            <div className="bg-transparent flex flex-col gap-11 max-h-[856px] overflow-y-scroll no-scrollbar whitespace-nowrap">
-              <Work />
-              <Work />
-              <Work />
-            </div>
+              <div className="bg-transparent flex flex-col gap-11">
+                {works.map((work) => {
+                  return <Work data={work} key={work.name} />
+                })}
+                
+              </div>
 
+            </div>
           </div>
         </section>
       </main>
 
-      <footer className="bg-slate-800 text-slate-100 p-4">
+      <footer className="bg-slate-800 text-slate-100 p-6">
         <div className="flex max-w-6xl mx-auto justify-between text-sm">
           <div>
             <h3 className="text-lg font-bold">É um prazer ter você aqui!</h3>
             <p>Esse site é melhor visualizado em desktops.</p>
-            <p>Tecnologias usadas: React.js, Tailwind CSS, Node.js.</p>
-            <p>Feito com amor 2024</p>
+            <p>Tecnologias usadas: React.js, Tailwind CSS.</p>
+            <p>Feito com amor ©2024</p>
           </div>
 
           <div className="flex gap-7">
-            <a href="#" className="flex items-center underline hover:text-slate-300">Github<GoArrowUpRight /></a>
-            <a href="#" className="flex items-center underline hover:text-slate-300">Linkedin<GoArrowUpRight /></a>
+            <a href="https://github.com/Nahblue" target="_blank" className="flex items-center underline hover:text-slate-300">Github<GoArrowUpRight /></a>
+            <a href="https://www.linkedin.com/in/ana-paula-costa-039b1181/" target="_blank" className="flex items-center underline hover:text-slate-300">Linkedin<GoArrowUpRight /></a>
           </div>
         </div>
       </footer>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,45 +1,45 @@
 import { GoArrowUpRight } from "react-icons/go";
 
-export function Work() {
+export function Work({ data }) {
+
   return (
     <div className="flex flex-col gap-5">
       <img
         src="https://s2-techtudo.glbimg.com/SSAPhiaAy_zLTOu3Tr3ZKu2H5vg=/0x0:1024x609/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/c/u/15eppqSmeTdHkoAKM0Uw/dall-e-2.jpg"
         alt=""
-        className="max-w-xl max-h-96 rounded-lg"
+        className="max-w-xl max-h-96 rounded-lg object-cover"
       />
 
-      <h2 className="text-4xl font-medium">Nome do projeto</h2>
+      <h2 className="text-4xl font-medium">{data.name}</h2>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
         <div>
           <span className="font-bold">Tecnologias:</span>
-          <p>CSS-HTML</p>
+          <p>{data.technologies}</p>
         </div>
 
-        <div className="flex gap-9">
-          <div>
-            <span className="font-bold">Projeto:</span>
-            <p>Pessoal</p>
-          </div>
-
-          <div>
-            <span className="font-bold">Função:</span>
-            <p>Desenvolvedor WEB</p>
-          </div>
+        <div>
+          <span className="font-bold">Projeto:</span>
+          <p>{data.project}</p>
         </div>
+
+        <div>
+          <span className="font-bold">Função:</span>
+          <p>{data.function}</p>
+        </div>
+
       </div>
 
       <div className="flex gap-9">
         <a
-          href=""
+          href={data.repositorie_link}
           className="flex justify-center items-center underline hover:text-slate-500"
         >
           Repositório
           <GoArrowUpRight />
         </a>
         <a
-          href=""
+          href={data.project_link}
           className="flex justify-center items-center underline hover:text-slate-500"
         >
           Projeto
